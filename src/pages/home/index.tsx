bd00459b3a749cea905fda8f4400fb3a136c4095
Nouani <nouanigabriel@hotmail.com>
2021-10-09 11:55:24 -0300

    :sparkles: FEATURE: Added global style
    Signed-off-by: Nouani <nouanigabriel@hotmail.com>

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-    return <h1>Home</h1>;
+    return <h1>John</h1>;
 };
 
 export default Home;
