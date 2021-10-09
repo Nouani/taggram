105b8a2c2ff6ed1f6c96cefccd93d55d465aa56b
Nouani <nouanigabriel@hotmail.com>
2021-10-09 01:53:12 -0300

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./style";
+
+const App: React.FC = () => {
+    return (
+        <Container>
+            <h1>Hello world</h1>
+        </Container>
+    );
+};
+
+export default App;
